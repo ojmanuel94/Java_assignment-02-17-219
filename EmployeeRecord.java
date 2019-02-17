99d821e431f28ad5a0b13c47c1e3f06ec0ebe4df
ojmanue94 <ojmanuel94@gmail.com>
2019-02-17 09:11:05 +0800

    initial push

---
+++ EmployeeRecord.java
@@ -0,0 +1,18 @@
+public class EmployeeRecord {
+	private String name; 
+	private String Position;
+	private int employeeNumber;
+	private double numberOfDaysSchedule;
+	private double numberOfDaysPresent;
+	private double totalAbsPercentage; 
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName ( String temp ) {
+		name = temp;
+	}
+
+}
---
+++ EmployeeRecordSample.java
@@ -0,0 +1,10 @@
+public class EmployeeRecordSample {
+	public static void main (String[] args) {
+		EmployeeRecord justineRecord = new EmployeeRecord();
+
+		justineRecord.setName("Justine Manuel");
+
+
+		System.out.println(justineRecord.getName());
+	} 
+}
